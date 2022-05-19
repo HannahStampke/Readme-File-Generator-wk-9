9489896ab2828e5fbc7640f3d4c707dfa3501d5c
Hannah Stampke <hannah.stampke@gmail.com>
2022-05-19 16:31:43 +0930

    Added prompts into index.js

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-// fs library package
+// Library packages (fs and inquirer)
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-
-
+// Questions to ask for input
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'Title',
+        },
+        {
+            type: 'input',
+            message: 'Write a project description',
+            name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions?',
+            name: 'Installation Instructions',
+        },
+        {
+            type: 'input',
+            message: 'What is the usage information?',
+            name: 'Usage Information',
+        },
+        {
+            type: 'input',
+            message: 'What are the contribution guidelines?',
+            name: 'Contribution Guidelines',
+        },
+        {
+            type: 'input',
+            message: 'What are the test instructions?',
+            name: 'Test Instructions',
+        },
+    ]);
+};
